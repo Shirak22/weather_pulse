f6cce09d3d39d034fef92b01024bc05e0a524dc2
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2026-09-04 12:23:41 +0200

    update SMHI API URL

--- js/data.js
+++ js/data.js
@@ -2,44 +2,60 @@ let validTimes;
 let selectedTime; 
 let timeline_days;
 
-const validTimes_URL = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/validtime.json`; 
+const SMHI_BASE = `https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1`;
+const times_URL = `${SMHI_BASE}/times.json`;
 
 const openData_settings = {
     downSample: config.general_settings.data.downSample,
 }
 
 const getValidTimes = async ()=> {
-    const res = await fetch(validTimes_URL);
+    const res = await fetch(times_URL);
+    if (!res.ok) throw new Error(`SMHI times failed: ${res.status}`);
     const data = await res.json();
-    return data.validTime;
+    return data.time;
 }
 
 const getData = async (time)=> {
     let validTime = time.replaceAll("-","").replaceAll(":","");
+    const ds = openData_settings.downSample;
 
-    //get coordinates
-    const coordinates_url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/multipoint.json?downsample=${openData_settings.downSample}`
-    const coordinates_response = await fetch(coordinates_url);
+    const coordinates_url = `${SMHI_BASE}/geotype/multipoint.json?downsample=${ds}`;
+    const windDirection_url = `${SMHI_BASE}/geotype/multipoint/time/${validTime}/parameter/wind_from_direction/data.json?downsample=${ds}&with-geo=false`;
+    const windspeed_url = `${SMHI_BASE}/geotype/multipoint/time/${validTime}/parameter/wind_speed/data.json?downsample=${ds}&with-geo=false`;
+    const temp_url = `${SMHI_BASE}/geotype/multipoint/time/${validTime}/parameter/air_temperature/data.json?downsample=${ds}&with-geo=false`;
+
+    const [coordinates_response, windDirection_response, windSpeed_response, temp_response] = await Promise.all([
+        fetch(coordinates_url),
+        fetch(windDirection_url),
+        fetch(windspeed_url),
+        fetch(temp_url),
+    ]);
+
+    for (const [name, res] of [
+        ["coordinates", coordinates_response],
+        ["wind_from_direction", windDirection_response],
+        ["wind_speed", windSpeed_response],
+        ["air_temperature", temp_response],
+    ]) {
+        if (!res.ok) throw new Error(`SMHI ${name} failed: ${res.status}`);
+    }
+
     const coordinates_data = await coordinates_response.json();
-    //wind direction 
-    const windDirection_url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/multipoint/validtime/${validTime}/parameter/wd/leveltype/hl/level/10/data.json?downsample=${openData_settings.downSample}&with-geo=false`
-    const windDirection_response = await fetch(windDirection_url);
     const windDirection_data = await windDirection_response.json();
-    
-    const windspeed_url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/multipoint/validtime/${validTime}/parameter/ws/leveltype/hl/level/10/data.json?downsample=${openData_settings.downSample}&with-geo=false`
-    const windSpeed_response = await fetch(windspeed_url);
     const windSpeed_data = await windSpeed_response.json();
-
-    const temp_url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/multipoint/validtime/${validTime}/parameter/t/leveltype/hl/level/2/data.json?downsample=${openData_settings.downSample}&with-geo=false`
-    const temp_response = await fetch(temp_url);
     const temp_data = await temp_response.json();
+
+    const coordinates =
+        coordinates_data.coordinates ??
+        coordinates_data.geometry?.coordinates;
 
     let rasterPoints = {
         validTime,
-        coordinates: coordinates_data.coordinates,
-        wind_direction: windDirection_data.timeSeries[0].parameters[0].values,
-        wind_speed:windSpeed_data.timeSeries[0].parameters[0].values,
-        temp_data:temp_data.timeSeries[0].parameters[0].values,
+        coordinates,
+        wind_direction: windDirection_data.timeSeries[0].data.wind_from_direction,
+        wind_speed: windSpeed_data.timeSeries[0].data.wind_speed,
+        temp_data: temp_data.timeSeries[0].data.air_temperature,
     }; 
 
 
@@ -63,7 +79,3 @@ const getData = async (time)=> {
     return rasterPoints;
     
 }
-
-
-
-
